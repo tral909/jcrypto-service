6a26865f3ae66f9022d8759058384a80bb5945cd
tral909 <indorm1992@mail.ru>
2021-04-16 16:44:31 +0300

    Add signature function with test private key

---
+++ src/main/java/io/github/tral909/jcrypto_service/backend/logic/SignatureService.java
@@ -0,0 +1,41 @@
+package io.github.tral909.jcrypto_service.backend.logic;
+
+import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+
+@Service
+public class SignatureService {
+
+	private final static String PRIVATE_KEY = "MIIBSwIBADCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoEFgIUH63HIQkL1yQw9FVN+/WjK6BoVyk=";
+	private final static String PUBLIC_KEY = "MIIBtzCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYQAAoGAOJS1cyf/XnlshzQhM86ccX34DAa4DbHzIuRbfBbaIHTlToKCJrsE0LiMsSzHLbpBu0DGashOAexLB0B25nMPQG6JHMmPs9g325Y8bQN/IMUTy+lRP5o1NWX1+LOqreRN1nXNUZVKZ9ELM+ZuD8+7aP/UwX2izU5kPv0e+Ll0kN4=";
+
+	@SneakyThrows
+	public byte[] sign(InputStream inputStream) {
+		// init key pair
+//		SecureRandom secureRandom2 = new SecureRandom();
+//		KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DSA");
+//		keyPairGen.initialize(1024);
+//		KeyPair keyPair = keyPairGen.generateKeyPair();
+
+		String payload = IOUtils.toString(inputStream, "UTF-8");
+		byte[] prKeyBytes = Base64.getDecoder().decode(PRIVATE_KEY);
+
+		PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(prKeyBytes);
+		KeyFactory keyFactory = KeyFactory.getInstance("DSA");
+		PrivateKey prKey = keyFactory.generatePrivate(keySpec);
+
+		Signature signature = Signature.getInstance("SHA256WithDSA");
+		signature.initSign(prKey);
+		signature.update(payload.getBytes());
+		return signature.sign();
+	}
+
+}
--- src/main/java/io/github/tral909/jcrypto_service/ui/CryptoSignatureView.java
+++ src/main/java/io/github/tral909/jcrypto_service/ui/CryptoSignatureView.java
@@ -1,12 +1,47 @@
 package io.github.tral909.jcrypto_service.ui;
 
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
+import elemental.json.Json;
+import io.github.tral909.jcrypto_service.backend.logic.SignatureService;
+
+import java.io.ByteArrayInputStream;
 
 @Route(value = "signature", layout = MainView.class)
 public class CryptoSignatureView extends VerticalLayout {
-    public CryptoSignatureView() {
-        add(new H1("Crypto Signature page will be soon!"));
+
+    public CryptoSignatureView(SignatureService signatureService) {
+        MemoryBuffer buffer = new MemoryBuffer();
+        Upload upload = new Upload(buffer);
+        Div output = new Div();
+
+        upload.addSucceededListener(event -> {
+            // убрать название файла после загрузки
+            upload.getElement().setPropertyJson("files", Json.createArray());
+            output.removeAll();
+
+            byte[] signed = signatureService.sign(buffer.getInputStream());
+
+            // ссылка для скачивания подписи
+            final StreamResource resource = new StreamResource(event.getFileName() + ".sig",
+                    () -> new ByteArrayInputStream(signed));
+            output.add(new Anchor(resource, "Download signature!"));
+        });
+
+        upload.addFileRejectedListener(event -> {
+            // убрать название файла после загрузки
+            upload.getElement().setPropertyJson("files", Json.createArray());
+            output.removeAll();
+            output.add(event.getErrorMessage());
+        });
+
+        upload.getElement().addEventListener("file-remove", event -> output.removeAll());
+
+        add(upload, output);
     }
 }
